72c1784579ff28532f4e3326e85cb1b7e2436168
tolmasbek <farmonovtt@gmail.com>
2020-04-22 16:32:01 +0500

    Created/Class/DataAccess/ConnectionString

---
+++ DataAccess/DataAccess.cs
@@ -0,0 +1,7 @@
+namespace _DataAccessSpace
+{
+    static public class DataAccess
+    {
+        public const string ConnectionString = "Data Source = TOLMASBEK-ПК; Initial Catalog = InterinProject; integrated Security = True";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace InterimProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.ReadKey();
+        }   
+    }
+}
