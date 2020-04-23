c7189ee8fc63e71215d3a76bc3a70af0ac1125a0
tolmasbek <farmonovtt@gmail.com>
2020-04-23 21:39:23 +0500

    finished

--- Program.cs
+++ Program.cs
@@ -1,37 +1,66 @@
 ﻿using System;
 using _AuthenticationSpace;
-using _AnketaSpace;
+using System.Data.SqlClient;
+using System.Data;
+using _DataAccessSpace;
 
 namespace InterimProject
 {
     class Program
     {
+        static void UserLogin(SqlConnection conn, Authentication.User user){
+                Users cmd = new Users();
+        
+                cmd.Show(conn,user.id);
+
+        }
+        static void AdminLogin(SqlConnection conn, Authentication.User user){
+                Admins cmd = new Admins();
+                cmd.Show(conn, user.id);
+
+        }
         static void Main(string[] args)
         {
+            Users cmd = new Users();
+            
+            Authentication auth = new Authentication();
+            SqlConnection conn = new SqlConnection(DataAccess.ConnectionString);
+          //  auth.AddingUser(conn);          cmd.Order(conn, 1);
             Console.ForegroundColor = ConsoleColor.Red;
             System.Console.WriteLine("CREDIT SYSTEM");
             Console.ForegroundColor = ConsoleColor.White;
-            System.Console.WriteLine("1 Вход\n2 Выход\nВаш выбор: ");
-            int ch = int.Parse(Console.ReadLine());
+            while(true){
+                System.Console.WriteLine("enter Вход\nexit Выход\nВаш выбор: ");
+            string ch = Console.ReadLine();
             switch(ch)
             {
-                case 1:
-                    System.Console.WriteLine("Введите Login: ");
-                    string login = Console.ReadLine();
-                    System.Console.WriteLine("Введите Password: ");
-                    string Pass = Console.ReadLine();
+                case "enter":
+                    
+                    Authentication.User user;
+                    user = auth.GetLoginAndPassword(conn);
+                    if(user.err!="")
+                    {
+                        System.Console.WriteLine(user.err);
+                        break;
+                    }
+                    if(user.role == "ADMIN")
+                     {
+                         System.Console.WriteLine("Вы вошли как админ ");
+                         AdminLogin(conn, user);
+                     }
+                     else{
+                         System.Console.WriteLine("Вы аошли как пользователь");
+                         UserLogin(conn, user);
+                     }
                 break;
-                case 2:
-
-                break;
+                case "exit":
+                        return;
                 default:
-                System.Console.WriteLine("Неверная команда: ");
+                System.Console.WriteLine("Неверная команда! ");
                 break;
             }
+            }
             
-
-
-            Console.ReadKey();
         }   
     }
 }
